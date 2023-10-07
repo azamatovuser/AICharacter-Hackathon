5482dd19444b3472fd05f70c9a4241dc74a543dd
azamat <azamatovuser@gmail.com>
2023-10-07 15:37:04 +0500

    patch 0.0.2

--- handlers/users/conversation_part.py
+++ handlers/users/conversation_part.py
@@ -4,12 +4,15 @@ import openai
 
 openai.api_key = os.environ.get('OPENAI_KEY')
 
+category = ''
+person = ''
 
-def answer_process(person, category, question):
+
+def answer_process(p, c, q):
     response = openai.Completion.create(
         model='text-davinci-003',
-        prompt=f"Bro you are {person} in {category} major and don't send dangerous information "
-               f"and you need to answer to this question - {question}. Also and don't begin conversation with ? or , or .",
+        prompt=f"Bro you are {p} in {c} major and don't send dangerous information "
+               f"and you need to answer to this question - {q}. Also and don't begin conversation with ? or , or .",
         max_tokens=1000,
     )
     if response['choices'][0]['text']:
@@ -20,10 +23,24 @@ def answer_process(person, category, question):
         answer.replace("`", "\\`")
         answer.replace("=", "\\=")
         return answer
-    text = 'Я совсем не понял вас :('
+    text = 'I am not sure I understand :('
     return text
 
 
 @dp.message_handler(content_types=['web_app_data'])
-async def hey(message: types.Message):
-    await message.answer('Accepted!')
+async def analyze_person(message: types.Message):
+    category = message.text['category']
+    person = message.text['person']
+    await message.answer(f"I am {person} who specializes in {category}. "
+                         f"It seems you want a conversation with me, "
+                         f"is there anything that I can help? :0")
+
+
+@dp.message_handler(content_types='text')
+async def conversation(message: types.Message):
+    if person == '':
+        await message.answer('First Person needs to be chosen')
+    else:
+        temporary_message = await message.answer("Well give me time..")
+        answer = answer_process(person, category, message.text)
+        await bot.edit_message_text(chat_id=message.from_user.id, message_id=temporary_message.message_id, text=answer)
